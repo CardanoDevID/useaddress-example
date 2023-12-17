3f35e67c42badcac17850b9dbad0afe24c8fee94
ValdryanIvandito <valdryan08@gmail.com>
2023-12-17 14:33:07 +0700

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,62 +1,67 @@
-import Head from "next/head";
-import { CardanoWallet, MeshBadge } from "@meshsdk/react";
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable jsx-a11y/alt-text */
+import { useAddress, useWallet, useWalletList } from "@meshsdk/react";
+import Image from "next/image";
 
 export default function Home() {
+  const wallets = useWalletList();
+  const { connect, disconnect, connected } = useWallet();
+  const address = useAddress();
+
+  const handleGetAddress = async (walletName: string) => {
+    console.log(`${walletName} clicked!`);
+    await connect(walletName);
+  };
+
   return (
-    <div className="container">
-      <Head>
-        <title>Mesh App on Cardano</title>
-        <meta name="description" content="A Cardano dApp powered my Mesh" />
-        <link
-          rel="icon"
-          href="https://meshjs.dev/favicon/favicon-32x32.png"
-        />
-        <link
-          href="https://meshjs.dev/css/template.css"
-          rel="stylesheet"
-          key="mesh-demo"
-        />
-      </Head>
+    <>
+      {/* example-2 */}
+      {!connected ? (
+        <div className="bg-black text-white h-screen flex justify-center items-center">
+          <div className="border border-white rounded-2xl w-60 h-48">
+            <h1 className="text-center text-2xl font-bold m-4">Wallet List:</h1>
+            <div className="flex justify-center items-center">
+              {wallets.map((wallet, i) => {
+                return (
+                  <div
+                    key={i}
+                    className="bg-slate-700 border border-transparent hover:border-white rounded-xl text-center font-bold m-2 p-2"
+                    // Trial-1
+                    // onClick={handleGetAddress}
 
-      <main className="main">
-        <h1 className="title">
-          <a href="https://meshjs.dev/">Mesh</a> Next.js
-        </h1>
-
-        <div className="demo">
-          <CardanoWallet />
+                    // Trial-2
+                    onClick={() => handleGetAddress(wallet.name)}
+                  >
+                    <Image
+                      src={wallet.icon}
+                      alt="wallet icon"
+                      width={50}
+                      height={50}
+                    />
+                    <p className="text-sm">{wallet.name}</p>
+                    <p className="text-xs">{wallet.version}</p>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
         </div>
-
-        <div className="grid">
-          <a href="https://meshjs.dev/apis" className="card">
-            <h2>Documentation</h2>
-            <p>
-              Our documentation provide live demos and code samples; great
-              educational tool for learning how Cardano works.
-            </p>
-          </a>
-
-          <a href="https://meshjs.dev/guides" className="card">
-            <h2>Guides</h2>
-            <p>
-              Whether you are launching a new NFT project or ecommerce store,
-              these guides will help you get started.
-            </p>
-          </a>
-
-          <a href="https://meshjs.dev/react" className="card">
-            <h2>React components</h2>
-            <p>
-              Useful React UI components and hooks, seamlessly integrate them
-              into your app, and bring the user interface to life.
-            </p>
-          </a>
+      ) : (
+        <div className="bg-black text-white h-screen flex justify-center items-center">
+          <div className="border border-white rounded-2xl h-48 px-4">
+            <h1 className="text-2xl font-bold">Get Address:</h1>
+            <p>{address}</p>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 rounded-xl p-2 font-bold"
+              onClick={() => {
+                disconnect();
+              }}
+            >
+              Disconnect
+            </button>
+          </div>
         </div>
-      </main>
-
-      <footer className="footer">
-        <MeshBadge dark={true} />
-      </footer>
-    </div>
+      )}
+    </>
   );
 }
